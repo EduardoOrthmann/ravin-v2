97040c13f360efd5b121f6bef556176ed4fe74b8
Eduardo <orthmann2811@gmail.com>
2023-09-04 20:42:07 -0300

    remove not null from address dto

--- src/main/java/com/example/ravin/domains/address/Address.java
+++ src/main/java/com/example/ravin/domains/address/Address.java
@@ -2,8 +2,6 @@ package com.example.ravin.domains.address;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
@@ -21,17 +19,9 @@ public class Address {
     private String state;
     private String city;
     private String zipCode;
-
-    @NotNull(message = "O bairro não pode ser nulo")
-    @NotBlank(message = "O bairro não pode estar em branco")
     private String neighborhood;
-
-    @NotNull(message = "A rua não pode ser nula")
-    @NotBlank(message = "A rua não pode estar em branco")
     private String street;
 
-    @NotNull(message = "O número não pode ser nulo")
-    @NotBlank(message = "O número não pode estar em branco")
     @Size(min = 1, max = 10, message = "O número deve ter entre 1 e 10 caracteres")
     private String number;
 
--- src/main/java/com/example/ravin/domains/customer/Customer.java
+++ src/main/java/com/example/ravin/domains/customer/Customer.java
@@ -5,7 +5,6 @@ import com.example.ravin.domains.person.Person;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +18,5 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Customer extends Person {
     @Embedded
-    @Valid
     private Address address;
 }
